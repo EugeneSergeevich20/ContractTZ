97407787abb3865f269914ddd70c0aa1ccef559a
EugeneSergeevich20 <eugene.chernitsyn15@gmail.com>
2022-09-01 12:34:53 +0400

    API для вывода этапов договора

---
+++ ContractTZ/Controllers/ContractStageController.cs
@@ -0,0 +1,33 @@
+﻿using ContractTZ.Services;
+using ContractTZ1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractTZ.Controllers
+{
+	[ApiController]
+	[Route("/api/Contracts/Get[controller]")]
+	public class ContractStageController : Controller
+	{
+
+		ApplicationContext db;
+
+		public ContractStageController(ApplicationContext context)
+		{
+			db = context;
+		}
+
+		[HttpGet("{contractId:int}")]
+		public IAsyncEnumerable<ContractStage> Get(int contractId)
+		{
+			return db.ContractStages.Where(c => c.contract.id == contractId).AsAsyncEnumerable();
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Content("Пожалуйста, добавьте в параметр запроса id договора");
+		}
+
+	}
+}
